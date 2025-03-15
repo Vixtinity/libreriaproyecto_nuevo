75cefa2ea777d65d2c7374dafca59c5b03fe8fcf
Ismael <08062006ismael@gmail.com>
2025-03-15 16:16:44 +0100

    arreglos paginas

--- src/main/resources/static/js/scriptLog.js
+++ src/main/resources/static/js/scriptLog.js
@@ -2,13 +2,13 @@ function validateLogin(event) {
   event.preventDefault(); // Prevenir el envío del formulario para manejar la validación con JavaScript
 
   // Obtener los valores del formulario
-  const username = document.getElementById('username').value;
+  const username = document.getElementById('nombre').value;
   const password = document.getElementById('password').value;
 
   // Validación básica (esto debería ser reemplazado por una verificación real de la base de datos)
   const validUsers = [
-    { username: 'admin', password: 'admin123', rol: 'admin' },
-    { username: 'user', password: 'user123', rol: 'user' }
+    { username: 'admin', password: 'admin', rol: 'admin' },
+    { username: 'user', password: 'admin', rol: 'user' }
   ];
 
   // Buscar el usuario en la lista de usuarios válidos
@@ -18,13 +18,7 @@ function validateLogin(event) {
   if (user) {
       if (user.rol === 'admin') {
           // Si el rol es admin, redirigir a la página de administración
-          window.location.href = '/admin-dashboard'; // Cambia esto a la URL de tu página de administración
-      } else {
-          // Si el rol no es admin, mostrar un mensaje de error o redirigir a otra página
-          alert('No tienes acceso a la administración.');
-      }
-  } else {
-      // Si el usuario no es válido, mostrar un mensaje de error
-      alert('Usuario o contraseña incorrectos.');
+          window.location.href = '/admin'; // Cambia esto a la URL de tu página de administración
   }
+}
 }
